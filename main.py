6efdba12c50997382796850da3d588c25313332d
SenyaZver <sashko1337@mail.ru>
2022-04-27 22:09:48 +0700

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import scipy
 
 from scipy.sparse import diags, linalg, spdiags
 from scipy.sparse.linalg import eigsh
@@ -17,25 +18,24 @@ np.set_printoptions(suppress=True)
 a = 1.1
 b = 0.8
 
-epsilon = 0.0000001
+epsilon = 0.0001
 
 # a = 0.9
 # b = 1.2
 
-h = 0.25
-
+h = 0.02
 
 grid_size = int(1 / h)
 
 
 def exact_function(x, y):
-    return np.cos(2 * x) * np.sin(y) + y * y * y
-    # return np.sin(y * y) + x * x * y
+    # return np.cos(2 * x) * np.sin(y) + y * y * y
+    return np.sin(y * y) + x * x * y
 
 
 def f(x, y):
-    return a * 4 * np.cos(2 * x) * np.sin(y) + b * np.cos(2 * x) * np.sin(y) - b * 6 * y
-    # return -a * 2 * y - b * (2 * np.cos(y * y) - 2 * y * np.sin(y * y))
+    # return a * 4 * np.cos(2 * x) * np.sin(y) + b * np.cos(2 * x) * np.sin(y) - b * 6 * y
+    return -a * 2 * y - b * (2 * np.cos(y * y) - 2 * y*y * np.sin(y * y))
 
 
 def createMatrix(grid_size):
@@ -72,26 +72,34 @@ def createB(h, grid_size, u):
     f_index2 = 1
 
     for i in range(0, b_size):
+        # print (f_index1, f_index2)
+        c_1 = -a / (h * h)
+        c_2 = -b / (h * h)
 
-        B[i] = -h * h * f(f_index1 / h, f_index2 / h)
+        B[i] = f(f_index1 * h, f_index2 * h)
+
 
         # adding boundary conditions
         if (f_index1 - 1) == 0:
-            B[i] += a * u[f_index1 - 1, f_index2]
-            # print(f_index1-1, f_index2)
+            B[i] += -c_1 * u[f_index1 - 1, f_index2]
+            print(f_index1-1, f_index2)
+
 
         if (f_index1 + 1) == grid_size - 1:
-            B[i] += a * u[f_index1 + 1, f_index2]
-            # print(f_index1+1, f_index2)
+            B[i] += -c_1 * u[f_index1 + 1, f_index2]
+            print(f_index1 + 1, f_index2)
+
 
         if (f_index2 - 1) == 0:
-            B[i] += b * u[f_index1, f_index2 - 1]
-            # print(f_index1, f_index2-1)
+            B[i] += -c_2 * u[f_index1, f_index2 - 1]
+            print(f_index1, f_index2-1)
+
 
         if (f_index2 + 1) == grid_size - 1:
-            B[i] += b * u[f_index1, f_index2 + 1]
-            # print(f_index1, f_index2+1)
+            B[i] += -c_2 * u[f_index1, f_index2 + 1]
+            print(f_index1, f_index2+1)
 
+        print()
         # calculating f indexes
         f_index1 = f_index1 + 1
         if f_index1 == grid_size - 1:
@@ -99,32 +107,61 @@ def createB(h, grid_size, u):
             f_index2 = f_index2 + 1
             if f_index2 == grid_size - 1:
                 f_index2 = 1
-    return B
 
+    # c_1 = -a / (h * h)
+    # c_2 = -b / (h * h)
+    # for j in range(1, grid_size - 1):
+    #     for i in range(1, grid_size - 1):
+    #         row = (j - 1) * (grid_size - 2) + i - 1
+    #
+    #         if u[j][i - 1] != np.inf:
+    #             B[row] -= c_1 * u[j][i - 1]
+    #
+    #         if u[j][i + 1] != np.inf:
+    #             B[row] -= c_1 * u[j][i + 1]
+    #
+    #         if u[j - 1][i] != np.inf:
+    #             B[row] -= c_2 * u[j - 1][i]
+    #
+    #         if u[j + 1][i] != np.inf:
+    #             B[row] -= c_2 * u[j + 1][i]
+    #
+    #         B[row] += f(i * h, j * h)
+
+
+
+
+    return B
 
 
 x = np.linspace(0, 1, grid_size + 1)
 u = np.zeros((grid_size + 1, grid_size + 1))
+u.fill(np.inf)
 
-u[0, :] = f(0, x[:])
-u[grid_size, :] = f(grid_size - 1, x[:])
 
-u[:, 0] = f(x[:], 0)
 
-u[:, grid_size] = f(x[:], grid_size - 1)
+# u[0] = exact_function(0, x[:])
+#
+# u[grid_size, :] = exact_function(grid_size, x[:])
+#
+# u[:, 0] = exact_function(x[:], 0)
+#
+# u[:, grid_size] = exact_function(x[:], grid_size)
 
-# AMatrix = createMatrix(grid_size+1)
-# print(AMatrix)
+for k in range(grid_size+1):
+    u[k, 0] = exact_function(k*h, 0)
+    u[k, grid_size] = exact_function(k*h, (grid_size)*h)
+    u[0, k] = exact_function(0, k*h)
+    u[grid_size, k] = exact_function((grid_size)*h, k*h)
 
 
 
 
 # creating sparse matrix
 
-main_diag = [2*(a+b)/(h*h) for _ in range((grid_size - 1) ** 2)]
-second_diag = [-1*a/(h*h) for _ in range((grid_size - 1) ** 2 - 1)]
-offset_diag = [-1*b/(h*h) for _ in range((grid_size - 1) ** 2 - (grid_size - 1))]
-
+main_diag = [2 * (a + b) / (h * h) for _ in range((grid_size - 1) ** 2)]
+second_diag = [-1 * a / (h * h) for _ in range((grid_size - 1) ** 2 - 1)]
+offset_diag = [-1 * b / (h * h) for _ in range((grid_size - 1) ** 2 - (grid_size - 1))]
 
 count = 0
 for i in range(len(second_diag)):
@@ -134,22 +171,14 @@ for i in range(len(second_diag)):
         continue
     count = count + 1
 
-
-
 offset = (grid_size - 1) ** 2 - len(offset_diag)
 A = diags([offset_diag, second_diag, main_diag, second_diag, offset_diag], [-offset, -1, 0, 1, offset])
-
-
-
-# print (A.todense())
-#
-
 
 # calculating largest eigenvalue
 eigenVector = np.ones((grid_size - 1) * (grid_size - 1))
 
 for i in range((grid_size - 1) * (grid_size - 1)):
-    eigenVector[i] = randint(0,10)
+    eigenVector[i] = randint(0, 10)
 
 lambda_old = eigenVector.dot(A.dot(eigenVector))
 lambda_new = max(abs(eigenVector))
@@ -166,21 +195,18 @@ while error > epsilon:
 
     error = abs(lambda_new - lambda_old)
     lambda_old = lambda_new
-    if (count % 10 == 0) & (h<0.02) :
+    if (count % 10 == 0) & (h < 0.02):
         sys.stdout.write(f"\r abs: {error}")
         sys.stdout.flush()
     count = count + 1
-if (h<0.02):
+if (h < 0.02):
     print()
-
 
 print("largest eigenvalue is", lambda_new)
 eigenSum = lambda_new
 
-
-# print (np.max(abs((A.dot(eigenVector) - eigenVector*eigenSum)[:] - np.zeros((grid_size - 1) * (grid_size - 1))[:])))
-if (np.max(A.dot(eigenVector) - eigenVector*eigenSum - np.zeros((grid_size - 1) * (grid_size - 1))) < 1):
-    print("eigenvalue is correct")
+# if (np.max(A.dot(eigenVector) - eigenVector*eigenSum - np.zeros((grid_size - 1) * (grid_size - 1))) < 1):
+#     print("eigenvalue is correct")
 
 
 # calculating smallest eigenvalue
@@ -191,7 +217,7 @@ reverseA = diags(new_diag) - A
 eigenVector = np.ones((grid_size - 1) * (grid_size - 1))
 
 for i in range((grid_size - 1) * (grid_size - 1)):
-    eigenVector[i] = randint(0,10)
+    eigenVector[i] = randint(0, 10)
 
 error = 100
 lambda_old = 0
@@ -206,26 +232,20 @@ while error > epsilon:
 
     error = abs(lambda_new - lambda_old)
     lambda_old = lambda_new
-    if (count % 10 == 0) & (h<0.02) :
+    if (count % 10 == 0) & (h < 0.02):
         sys.stdout.write(f"\r abs: {error}")
         sys.stdout.flush()
     count = count + 1
 
-if (h<0.02):
+if (h < 0.02):
     print()
-
-
 
 print("smallest eigenvalue is", eigenSum - lambda_new)
 
-
-# print (np.max(abs((A.dot(eigenVector) - eigenVector*(eigenSum - lambda_new))[:] - np.zeros((grid_size - 1) * (grid_size - 1))[:])))
-# print (np.max(A.dot(eigenVector) - eigenVector*(eigenSum - lambda_new) - np.zeros((grid_size - 1) * (grid_size - 1))))
-
-if (np.max(A.dot(eigenVector) - eigenVector*(eigenSum - lambda_new) - np.zeros((grid_size - 1) * (grid_size - 1)))<1):
-    print("eigenvalue is correct")
-
-print()
+# if (np.max(A.dot(eigenVector) - eigenVector*(eigenSum - lambda_new) - np.zeros((grid_size - 1) * (grid_size - 1)))<1):
+#     print("eigenvalue is correct")
+#
+# print()
 
 
 # eigvals = sorted(lg.eigvalsh(A.todense()))
@@ -240,56 +260,89 @@ print()
 # print("smallest true eigenvalue is " + str(norm - linalg.eigsh(norm * diags(np.ones((grid_size - 1) ** 2), 0) - A, k=1, return_eigenvectors=False)[0]))
 
 
-
-
 eigenSum = eigenSum + eigenSum - lambda_new
 
 # solving the system
-# B = createB(h, grid_size+1, u)
-#
-# calculated_u = np.ones(((grid_size - 1) ** 2))
-# previous_calculated_u = np.ones(((grid_size - 1) ** 2))
-#
-# w = 2/eigenSum
-# tau = B - A.dot(calculated_u)
-#
-# error = 1
-# while np.linalg.norm(tau) > epsilon:
-#     temp = calculated_u
-#     calculated_u = previous_calculated_u + w * (B - A.dot(previous_calculated_u))
-#     previous_calculated_u = temp
-#
-#     tau = B - A.dot(calculated_u)
-#
-#
-#
+B = createB(h, grid_size + 1, u)
+print(B)
+
+calculated_u = np.ones(((grid_size - 1) ** 2))
+previous_calculated_u = np.ones(((grid_size - 1) ** 2))
+
+w = 2 / eigenSum
+tau = B - A.dot(calculated_u)
+
+error = 1
+while np.linalg.norm(tau) > epsilon:
+    temp = calculated_u
+    calculated_u = previous_calculated_u + w * (B - A.dot(previous_calculated_u))
+    previous_calculated_u = temp
+
+    tau = B - A.dot(calculated_u)
+
+
+temp = scipy.sparse.csr_matrix(A)
+realSolution = scipy.sparse.linalg.spsolve(temp, B)
+
+
 # print("solution is")
 # print(calculated_u)
-# print("")
+#
 # print("real solution is")
-# test = np.linalg.solve(A.todense(), B)
-# print(test)
+# print(realSolution)
+
+
+index = -1
+tempIndex = 0
+for i in range(1, grid_size):
+    index += 1
+    for j in range(1, grid_size):
+        u[i][j] = calculated_u[index + tempIndex]
+
+        tempIndex += grid_size-1
+    tempIndex = 0
+
+
+# creating calculated graph
+xy = np.linspace(0, 1, grid_size+1)
+X, T = np.meshgrid(xy, xy)
+
+X_exact, T_exact = np.meshgrid(xy, xy)
+
+
+
+CalculatedGraph = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, T, u, 30, cmap='binary')
+ax.plot_surface(X, T, u, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('u')
+ax.invert_xaxis()
+ax.set_title('Calculated Solution')
+
+
 
 
 # Creating exact graph
+xy = np.linspace(0, 1, grid_size+1)
+X, T = np.meshgrid(xy, xy)
 
-# grid_size = int(1 / h) + 1
 
-# xy = np.linspace(0, 1, grid_size)
-# X, T = np.meshgrid(xy, xy)
-#
-#
 # X_exact, T_exact = np.meshgrid(xy, xy)
-# U_exact = exact_function(T, X)
-#
-# ExactGraph = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.contour3D(X_exact, T_exact, U_exact, 30, cmap='binary')
-# ax.plot_surface(X_exact, T_exact, U_exact, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('u')
-# ax.invert_xaxis()
-# ax.set_title('Exact Solution')
-#
-# plt.show()
+U_exact = exact_function(T, X)
+
+print(np.max(np.abs(u[:][:] - U_exact[:][:])))
+
+
+ExactGraph = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X_exact, T_exact, U_exact, 30, cmap='binary')
+ax.plot_surface(X_exact, T_exact, U_exact, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('u')
+ax.invert_xaxis()
+ax.set_title('Exact Solution')
+
+plt.show()
